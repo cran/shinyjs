bdc862527f36e8fd5e2db4f5d77657398682f839
Dean Attali <daattali@gmail.com>
2026-01-15 06:10:08 +0000

    version 2.1.1

---
+++ R/shinyjs-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' Defunct functions in shinyjs
+#'
+#' - **colourInput()** Moved to the \{colourpicker\} package
+#' - **updateColourInput()** Moved to the \{colourpicker\} package
+#' - **colourPicker()** Moved to the \{colourpicker\} package
+#' - **runExample()** Use `shiny::runExample(package = "shinyjs")`
+#'
+#' @name shinyjs-defunct
+NULL
